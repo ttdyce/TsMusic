a89605942a21bd2e7032b469dd3d08748f04533f
TT <ttdyce@gmail.com>
2020-08-20 17:02:34 +0800

    Update settings
    - dependencies
    - vue settings
    - font

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    module: {
+      rules: [
+        { // fix webpack with NeteaseCloudMusicApi (in pac-proxy-agent) error
+          test: /node_modules[\/\\](iconv-lite)[\/\\].+/,
+          resolve: {
+            aliasFields: ["main"],
+          },
+        },
+      ],
+    },
+  },
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+    },
+  },
+};
